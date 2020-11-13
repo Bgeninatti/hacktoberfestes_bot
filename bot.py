1b0dd6a68c0de7c6327a33e537267370d1c25fc5
Bruno Geninatti <brunogeninatti@gmail.com>
2020-11-13 20:26:59 +0000

    registration bot by private message

--- bot.py
+++ bot.py
@@ -16,11 +16,11 @@ CHANNEL = config["DEFAULT"]["Channel"]
 ADMIN_CHANNEL = config["DEFAULT"]["AdminChannel"]
 ROLE = config["DEFAULT"]["Role"]
 LIST = config["DEFAULT"]["List"]
+VALIDATION_FIELD = config["DEFAULT"]["ValidationField"]
 
-RIDS = set(pd.read_csv(LIST)["Order ID"].apply(int))
+RIDS = set(pd.read_csv(LIST)[VALIDATION_FIELD].str.lower())
 
 bot = commands.Bot(command_prefix="!")
-
 
 class RegistrationStatus(Enum):
     OK = 0
@@ -33,25 +33,24 @@ def get_ready_tickets():
     with open("ready.csv", "r") as f:
         reader = csv.reader(f)
         try:
-            registered = set(int(line[0]) for line in reader)
+            registered = set(line[0] for line in reader)
         except ValueError:
             print("Error converting lines to integeres")
     return registered
 
 
-def register_user(n, msg):
+def register_user(ticket_id, msg):
     registered = get_ready_tickets()
     remaining = RIDS - registered
-    if n in remaining:
+    if ticket_id in remaining:
         with open("ready.csv", "a") as f:
             csv_writer = csv.writer(f)
-            csv_writer.writerow([str(n), msg.author, msg.created_at])
+            csv_writer.writerow([ticket_id, msg.author, msg.created_at])
         return RegistrationStatus.OK
+    elif ticket_id in registered:
+        return RegistrationStatus.ALREADY_REGISTERED
     else:
-        if n in registered:
-            return RegistrationStatus.ALREADY_REGISTERED
-        else:
-            return RegistrationStatus.NOT_FOUND
+        return RegistrationStatus.NOT_FOUND
 
 
 @bot.command(name="estado", help="Comando para ver el estado actual de regitros", pass_context=True)
@@ -65,51 +64,40 @@ async def estado(ctx):
 
 
 @bot.command(name="registro", help="Comando de registro", pass_context=True)
-async def registro(ctx, ticket_number: str):
+async def registro(ctx, ticket_id: str):
 
-    if str(ctx.channel) != CHANNEL:
-        # Remove messages when not on the specific registration channel
-        await discord.Message.delete(ctx.message)
+    # Load the server
+    server = bot.get_guild(int(GUILD))
+    # FIXME: This obscure hack to update the member list
+    server._members = {m.id: m async for m in server.fetch_members()}
+    # Load special role to give permissions
+    role = discord.utils.get(server.roles, name=ROLE)
+    member = server.get_member(ctx.message.author.id)
+
+    msg = None
+
+    if not member:
+        msg = f"Tenes que ingresar al server de PyConAr 2020 antes de registrarte."
+        await ctx.send(msg)
     else:
-        number = None
-        # Allow people adding the '#' at the beginning of the ticket number
-        if ticket_number.startswith("#"):
-            ticket_number = ticket_number[1:].strip()
-        try:
-            number = int(ticket_number)
-        except ValueError:
-            await ctx.send("Ticket incorrecto, ingresa solo números.\n\t" "`!registro 123456`")
-
-        # Load special role to give permissions
-        role = discord.utils.get(ctx.message.author.guild.roles, name=ROLE)
-
-        msg = None
-        status = register_user(number, ctx.message)
+        status = register_user(ticket_id.lower(), ctx.message)
 
         if status == RegistrationStatus.OK:
             msg = f"Usuario {ctx.message.author.mention} registrado! :)"
             # TODO: Check if user already has the role
-            await ctx.author.add_roles(role)
+            await member.add_roles(role)
 
             # Send final response
             await ctx.send(msg)
         elif status == RegistrationStatus.ALREADY_REGISTERED:
             msg = "Ticket ya registrado"
-            message = await ctx.send(msg)
-            await asyncio.sleep(2)
-            await discord.Message.delete(message)
+            await ctx.send(msg)
         elif status == RegistrationStatus.NOT_FOUND:
-            msg = "El ticket no existe, revise su número."
-            message = await ctx.send(msg)
-            await asyncio.sleep(2)
-            await discord.Message.delete(message)
+            msg = "El ticket no existe. Estas seguro que lo ingresaste correctamente?"
+            await ctx.send(msg)
         else:
             # This should never happens, I Promise
             pass
-
-        # Remove command after parsing it
-        await discord.Message.delete(ctx.message)
-
 
 # Removing help command
 bot.remove_command("help")
