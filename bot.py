c2cd0d35cdb9787213ef8dfda21599476c59ea7d
Bruno Geninatti <brunogeninatti@gmail.com>
2020-11-16 09:32:31 +0000

    Activity monitor for messages and reactions

--- bot.py
+++ bot.py
@@ -1,39 +1,17 @@
 import asyncio
-import configparser
 import csv
 import os
 from enum import Enum
 
 import discord
 import pandas as pd
+from config import load_config
 from discord.ext import commands
 from logger import get_logger
 
 LOGGER = get_logger(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_FILE = os.path.join(BASE_DIR, 'config.ini')
-
-
-if os.path.exists(CONFIG_FILE):
-    # This is for local deploy with a config file
-    config = configparser.ConfigParser()
-    config.read(CONFIG_FILE)
-    LOGGER.info("Configuration loaded from config file", extra={'file': CONFIG_FILE})
-else:
-    # This is for Heroku where configvars are set as environ variables
-    config = {
-        "DEFAULT": {
-            "Token": os.environ.get("Token"),
-            "Guild": os.environ.get("Guild"),
-            "Channel": os.environ.get("Channel"),
-            "AdminChannel": os.environ.get("AdminChannel"),
-            "Role": os.environ.get("Role"),
-            "List": os.environ.get("List"),
-            "ValidationField": os.environ.get("ValidationField")
-        }
-    }
-    LOGGER.info("Configuration loaded from environment variables")
-
+config = load_config()
 
 TOKEN = config["DEFAULT"]["Token"]
 GUILD = config["DEFAULT"]["Guild"]
@@ -139,8 +117,10 @@ async def registro(ctx, ticket_id: str):
             # This should never happens, I Promise
             pass
 
+
 # Removing help command
 bot.remove_command("help")
+
 
 # Starting the bot
 bot.run(TOKEN)
